65b102324baff80c4dd54696aa44c8470ad93830
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-07-02 15:31:45 +0900

    style: 💄 Add formatting for config files

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  rootDir: ".",
-  roots: ["<rootDir>/src/", "<rootDir>/test/"],
-  testMatch: ["<rootDir>/test/*.ts"], // ignore emails
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  testMatch: ['<rootDir>/test/*.ts'], // ignore emails
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import nodeResolve from '@rollup/plugin-node-resolve'
-import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 const config = {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
-    format: "umd",
+    format: 'umd',
     name: 'FilterValidateEmail',
     sourcemap: true,
     compact: isProd,
     indent: !isProd,
-    exports: "named",
-    file: `dist/filter-validate-email.${isProd ? "min.js" : "js"}`,
+    exports: 'named',
+    file: `dist/filter-validate-email.${isProd ? 'min.js' : 'js'}`,
   },
   plugins: [
     nodeResolve({
-      extensions: [".ts"],
+      extensions: ['.ts'],
     }),
     babel({
-      include: ["src/**/*"],
-      exclude: "node_modules/**",
-      extensions: [".ts"],
+      include: ['src/**/*'],
+      exclude: 'node_modules/**',
+      extensions: ['.ts'],
     }),
     isProd && terser(),
   ],
-}
+};
 
-export default config
+export default config;
